031d7cc7bae15bd3d36f8310597b45cd363ac0bf
Alex <kushnarenko.alex@gmail.com>
2017-05-08 11:52:57 +0300

    r rework

--- stubproject/rScript/script.R
+++ stubproject/rScript/script.R
@@ -2,8 +2,7 @@ print(commandArgs(trailingOnly=TRUE))
 
 args <- commandArgs(trailingOnly = TRUE)
 
-
-#remove.packages(c("imager")) 
+#remove.packages(c("imager"))
 #source("https://bioconductor.org/biocLite.R")
 #if(!require(stringi)){install.packages("stringi")}
 #if(!require(jpeg)){install.packages("jpeg")}
@@ -18,9 +17,11 @@ library(EBImage)
 parts <- 10
 pxlsize <- 5
 
-print("START")
- i1 <- load.image(args[1])
- i2 <- load.image(args[2])
+i1 <- load.image('D:/Uni/Diplom/SpringStubProject/stubproject/rScript/1.jpg')
+i2 <- load.image('D:/Uni/Diplom/SpringStubProject/stubproject/rScript/2.jpg')
+#print("START")
+# i1 <- load.image(args[1])
+# i2 <- load.image(args[2])
 
 # i1 <- load.image('D:/temp/StubSpringProject/stubproject/rScript/5.jpg')
 # i2 <- load.image('D:/temp/StubSpringProject/stubproject/rScript/6.jpg')
@@ -43,58 +44,53 @@ normalize_image <- function(img, parts_number){
   }
 }
 
-
-
 imglist1 <- split_image(i1, parts)
 imglist2 <- split_image(i2, parts)
-
 
 i11 <- i1
 i21 <- i2
 
-
-
 x1 <- imsplit(i11,"c") %>% add
 x2 <- imsplit(i21,"c") %>% add
 
-thmb1 <- resize(x1,200,200) 
-thmb2 <- resize(x2,200,200) 
+# thmb1 <- resize(x1,200,200) 
+# thmb2 <- resize(x2,200,200) 
 
-thmb3 <- resize(thmb1,dim(i11)[1],dim(i11)[2]) 
-thmb4 <- resize(thmb2,dim(i21)[1],dim(i21)[2]) 
+# thmb3 <- resize(thmb1,dim(i11)[1],dim(i11)[2]) 
+# thmb4 <- resize(thmb2,dim(i21)[1],dim(i21)[2]) 
 
+thmb3 <-
+thmb4 <-
 
 
 # print(imcol(i11,1))
 
 # normalize_image(x1,3)
 
-
 #filter <- as.cimg(function(x,y) sign(x-5),10,10)
 # plot(convolve(x1,filter),main="x1")
 # plot(convolve(x2,filter),main="x1")
 
-
 sub1 <- x1-thmb3
 sub2 <- x2-thmb4
-
 
 binary1 <- round(sub1/max(sub1),2)
 binary2 <- round(sub2/max(sub2),2)
 
-
 thresh1 <- thresh(binary1, 10, 10, 0.02)
 thresh2 <- thresh(binary2, 10, 10, 0.02)
+#thresh1 <- binary1 > 15
+#thresh2 <- binary2 > 15
 
 res <- i1
 
 for(x in 1:(dim(i1)[1]-1)){
    for(y in 1:(dim(i1)[2]-1)){
         if(thresh2[x,y,1,1] == 1){
-         # print(paste(x, y, sep="-", collapse=", "))
+          print(paste(x, y, sep="-", collapse=", "))
           res[x,y,1, 1] <- i2[x,y,1,1]
           res[x,y,1, 2] <- i2[x,y,1,2]
-          res[x,y,1, 3] <- i1[x,y,1,3]
+          res[x,y,1, 3] <- i2[x,y,1,3]
         } else {
           res[x,y,1, 1] <- i1[x,y,1,1]
           res[x,y,1, 2] <- i1[x,y,1,2]
@@ -102,35 +98,35 @@ for(x in 1:(dim(i1)[1]-1)){
         }
    }
 }
-save.image(res,args[3])
-pdf('D:/temp/StubSpringProject/stubproject/rScript/filename3.pdf')
+#save.image(res,args[3])
+pdf('D:/Uni/Diplom/SpringStubProject/stubproject/rScript/filename3.pdf')
+# pdf('D:/temp/StubSpringProject/stubproject/rScript/filename3.pdf')
 plot(i1)
 plot(i2)
 plot(res)
 dev.off()
 
 print("DONE")
-# old.par <- par(mfrow=c(5, 5))
+old.par <- par(mfrow=c(3, 4))
 
-# plot(i11, main="image 1 part")
-# plot(i21, main="image 2 part")
+plot(i11, main="image 1 part")
+plot(i21, main="image 2 part")
 
 # plot(thmb1, main="thmb1")
 # plot(thmb2, main="thmb2")
-# plot(thmb3, main="thmb3")
-# plot(thmb4, main="thmb4")
+plot(thmb3, main="thmb3")
+plot(thmb4, main="thmb4")
 
-# plot(x1, main="x1")
-# plot(x2, main="x2")
+plot(x1, main="x1")
+plot(x2, main="x2")
 
-# plot(sub1, main="x1-thmb3")
-# plot(sub2, main="x2-thmb4")
+plot(sub1, main="x1-thmb3")
+plot(sub2, main="x2-thmb4")
 
 # plot(binary1 , main="Binary11")
 # plot(binary2 , main="Binary21")
 
-# plot(thresh1 , main="Sub1")
-# plot(thresh2 , main="Sub2")
+plot(thresh1 , main="Sub1")
+plot(thresh2 , main="Sub2")
 
-# par(old.par)
-
+par(old.par)
