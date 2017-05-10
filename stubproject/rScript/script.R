4b532ed3c30dc9240c510f51793afb3d5a259e37
oleksandr_kushnarenko@epam.com <oleksandr_kushnarenko@epam.com>
2017-05-10 13:43:20 +0300

    R

--- stubproject/rScript/script.R
+++ stubproject/rScript/script.R
@@ -9,12 +9,14 @@ args <- commandArgs(trailingOnly = TRUE)
 #if(!require(imager)){install.packages("imager")}
 #if(!require(EBImage)){biocLite("EBImage")}
 #if(!require(spatstat)){install.packages("spatstat")}
+#if(!require(SDMTools)){install.packages("SDMTools")}
 
 library(stringi)
 library(jpeg)
 library(imager)
 library(EBImage)
 library(spatstat)
+library(SDMTools)
 
 parts <- 10
 pxlsize <- 5
@@ -25,8 +27,8 @@ pxlsize <- 5
 # i1 <- load.image(args[1])
 # i2 <- load.image(args[2])
 
-i1 <- load.image('D:/temp/StubSpringProject/stubproject/rScript/15.jpg')
-i2 <- load.image('D:/temp/StubSpringProject/stubproject/rScript/16.jpg')
+i1 <- load.image('D:/temp/StubSpringProject/stubproject/rScript/7.jpg')
+i2 <- load.image('D:/temp/StubSpringProject/stubproject/rScript/8.jpg')
 
 split_image <- function(img,parts_number){
   x <- imsplit(img,'x', parts_number)
@@ -64,8 +66,8 @@ x2 <- imsplit(i21,"c") %>% add
 m1 <- matrix(i1,dim(i1)[1],dim(i1)[2])
 m2 <- matrix(i2,dim(i2)[1],dim(i2)[2])
 
-thmb3 <- as.matrix(blur(as.im(m1), sigma=2))
-thmb4 <- as.matrix(blur(as.im(m2), sigma=2))
+thmb3 <- as.matrix(blur(as.im(m1), 10))
+thmb4 <- as.matrix(blur(as.im(m2), 10))
 
 thmb3 <- as.im(thmb3)
 thmb4 <- as.im(thmb4)
@@ -76,31 +78,39 @@ thmb4 <- as.im(thmb4)
 # normalize_image(x1,3)
 
 #filter <- as.cimg(function(x,y) sign(x-5),10,10)
-# plot(convolve(x1,filter),main="x1")
-# plot(convolve(x2,filter),main="x1")
 
-sub1 <- as.im(as.matrix(x1)-thmb3$v)
-sub2 <- as.im(as.matrix(x2)-thmb4$v)
+sub1 <- as.matrix(x1)-thmb3$v
+sub2 <- as.matrix(x2)-thmb4$v
 
-# binary1 <- round(sub1/max(sub1),2)
-# binary2 <- round(sub2/max(sub2),2)
+# greyscale1 <- round(sub1/max(sub1),2)
+# greyscale2 <- round(sub2/max(sub2),2)
 
-# thresh1 <- thresh(sub1, 10, 10, 0.02)
-# thresh2 <- thresh(sub2, 10, 10, 0.02)
+thresh1 <- thresh(sub1, 10, 10, 0.02)
+thresh2 <- thresh(sub2, 10, 10, 0.02)
 
-thresh1 <- sub1 < 1 & sub1> -1
-thresh2 <- sub2 < 1 & sub2> -1
+# thresh1 <- greyscale1$v > otsu(greyscale1$v, range = c(-1, 1))
+# thresh2 <- greyscale2$v > otsu(greyscale2$v, range = c(-1, 1))
+
+thresh1 <- sub1 > otsu(sub1, range = c(-3, 3))
+thresh2 <- sub2 > otsu(sub2, range = c(-3, 3))
+
+# thresh2 <- !thresh2
+
 
 res <- i1
 
-for(x in 1:(dim(i1)[1]-1)){
-   for(y in 1:(dim(i1)[2]-1)){
-     print(paste(x, y, sep="-", collapse=", "))
-        if(thresh2[x,y,1,1] == 1){
+n <- 1
+
+for(x in n:(dim(i1)[1]-n)){
+   for(y in n:(dim(i1)[2]-n)){
+        print(paste(x, y, sep="-", collapse=", "))
+     if(thresh2[x,y] == FALSE){
+     # if(thresh1[x,y] == FALSE & mean(thresh2[x-n:x+n,y-n:y+n])>mean(thresh1[x-n:x+n,y-n:y+n])){
+       # if(mean(thresh2[x-n:x+n,y-n:y+n])>mean(thresh1[x-n:x+n,y-n:y+n])){
           res[x,y,1, 1] <- i2[x,y,1,1]
           res[x,y,1, 2] <- i2[x,y,1,2]
           res[x,y,1, 3] <- i2[x,y,1,3]
-        } 
+        }
         # else {
           # res[x,y,1, 1] <- i1[x,y,1,1]
           # res[x,y,1, 2] <- i1[x,y,1,2]
@@ -130,13 +140,13 @@ plot(thmb4, main="thmb4")
 plot(x1, main="x1")
 plot(x2, main="x2")
 
-plot(sub1, main="x1-thmb3")
-plot(sub2, main="x2-thmb4")
+plot(as.im(sub1), main="x1-thmb3")
+plot(as.im(sub2), main="x2-thmb4")
 
-# plot(binary1 , main="Binary11")
-# plot(binary2 , main="Binary21")
+plot(as.im(greyscale1$v) , main="greyscale11")
+plot(as.im(greyscale2$v) , main="greyscale21")
 
-plot(thresh1 , main="thresh1")
-plot(thresh2 , main="thresh2")
+plot(as.raster(thresh1) , main="thresh1")
+plot(as.raster(thresh2) , main="thresh2")
 
 par(old.par)
